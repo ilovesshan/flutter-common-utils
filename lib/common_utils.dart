96776b380dc1e1c3fc61529be37c38d062581f18
ilovesshan <2665939276@qq.com>
2023-05-22 17:47:41 +0800

    refactor: export file

--- lib/common_utils.dart
+++ lib/common_utils.dart
@@ -32,34 +32,42 @@ export 'package:fluwx_no_pay/fluwx_no_pay.dart';
 export 'package:share_plus/share_plus.dart';
 export 'package:qr_flutter/qr_flutter.dart';
 export 'package:url_launcher/url_launcher.dart';
+
 // export 'package:jpush_flutter/jpush_flutter.dart';
 // export 'package:jverify/jverify.dart';
 export 'package:flutter_xupdate/flutter_xupdate.dart';
 
+
 /// 函数类型
 export 'src/types/types.dart';
+
 
 /// 模型
 export 'src/mode/event_bus_call_back_model.dart';
 export 'src/mode/common_bottom_sheet_result_model.dart';
 export 'src/mode/gaode_pois_model.dart';
 
+
 /// 网络请求相关
 export 'src/service_utils/http_helper.dart';
 export 'src/service_utils/http_helper_config.dart';
 export 'src/service_utils/service_urls.dart';
 
+
 /// 配置信息
 export 'src/config/config.dart';
+
 
 /// view
 export 'src/view/base_view.dart';
 export 'src/view/view_state.dart';
 
+
 /// view_mode
 export 'src/view_model/base_view_model.dart';
 export 'src/view_model/simple_view_model.dart';
 export 'src/view_model/refresh_view_model.dart';
+
 
 /// 通用 widget
 export 'src/widgets/image_preview.dart';
@@ -72,29 +80,34 @@ export 'src/widgets/marquee_widget.dart';
 export 'src/widgets/gaode_map_widget.dart';
 export 'src/widgets/nav_bar.dart';
 
+
 /// 公共页面
 export 'src/pages/scankit_page.dart';
 export 'src/pages/web_view.dart';
 export 'src/pages/web_view_plugin.dart';
 export 'src/pages/un_know_page.dart';
 
+
 /// 工具包
 export 'src/utils/app_initialize.dart';
-export 'src/utils/log_util.dart';
 export 'src/utils/color_util.dart';
-export 'src/utils/text_util.dart';
-export 'src/utils/image_picker_util.dart';
 export 'src/utils/common_cache.dart';
-export 'src/utils/file_saver_util.dart';
-export 'src/utils/time_util.dart';
-export 'src/utils/event_bus_util.dart';
 export 'src/utils/connectivity_util.dart';
-export 'src/utils/file_upload_util.dart';
-export 'src/utils/qr_scanner_util.dart';
 export 'src/utils/encrypt_util.dart';
+export 'src/utils/event_bus_util.dart';
+export 'src/utils/file_saver_util.dart';
+export 'src/utils/file_upload_util.dart';
 export 'src/utils/function_proxy.dart';
+export 'src/utils/image_picker_util.dart';
 export 'src/utils/location_util.dart';
-export 'src/utils/wx_util.dart';
+export 'src/utils/log_util.dart';
 export 'src/utils/map_navigation_util.dart';
+export 'src/utils/permission_util.dart';
+export 'src/utils/qr_scanner_util.dart';
+export 'src/utils/record_util.dart';
+// export 'src/utils/shared_preferences_util.dart';
+export 'src/utils/text_util.dart';
+export 'src/utils/time_util.dart';
 export 'src/utils/toast_util.dart';
+export 'src/utils/wx_util.dart';
 export 'src/utils/x_update_util.dart';
